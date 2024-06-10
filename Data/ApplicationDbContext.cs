494db7207df9f592bd50cb50d7492f00eef22467
AbanoubSaweris <abanoubphelopos12@gmail.com>
2024-06-10 21:00:20 +0200

    add connection string and implement the category table use SQL Server

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shopy.Models;
+
+namespace Shopy.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options):base(options){ }
+    private DbSet<Category> Categories { get; set; }
+}
