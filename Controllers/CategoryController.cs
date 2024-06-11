d4ff3dd19cb7c08f31433e9d8fbf4ecd6d95ee42
AbanoubSaweris <abanoubphelopos12@gmail.com>
2024-06-11 15:28:49 +0200

    Admin DashBoard initialized and integrated with the project

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Shopy.Data;
+using Shopy.Models;
+
 namespace Shopy.Controllers;
 
 public class CategoryController : Controller
@@ -11,9 +13,72 @@ public class CategoryController : Controller
         _context = context;
     }
     // GET
+    [HttpGet]
     public IActionResult Index()
     {
         var Categories = _context.Categories.ToList();
         return View(Categories);
+    }
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    }
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Create(Category category)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Add(category);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        return View();
+    }
+    
+    [HttpGet]
+    public IActionResult Edit(int? Id)
+    {
+        if (Id == null || Id == 0)
+        {
+            return NotFound();
+        }
+        var category = _context.Categories.FirstOrDefault(category => category.Id==Id);
+        return View(category);
+    }
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Edit(Category category)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Update(category);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        return View();
+    }
+    [HttpGet]
+    public IActionResult Delete(int? Id)
+    {
+        if (Id == null || Id == 0)
+        {
+            return NotFound();
+        }
+        Category?  category = _context.Categories.FirstOrDefault(category => category.Id==Id);
+        return View(category);
+    }
+    [HttpPost, ActionName("Delete")]
+    public IActionResult DeletePost(int? Id)
+    {
+        Category? category = _context.Categories.FirstOrDefault(category => category.Id==Id);
+        if (category == null)
+        {
+            return NotFound();
+        }
+        _context.Categories.Remove(category);
+        _context.SaveChanges();
+        return RedirectToAction("Index");
     }
 }
--- Models/Category.cs
+++ Models/Category.cs
@@ -6,6 +6,6 @@ public class Category
 {
     public int Id { get; set; }
     [Required] public string Name { get; set; } = string.Empty;
-    public string Description { get; set; } = string.Empty;
-    public DateTime CreatedTime { get; set; }
+    [Required]public string Description { get; set; } = string.Empty;
+    public DateTime CreatedTime { get; set; } = DateTime.Now;
 }
